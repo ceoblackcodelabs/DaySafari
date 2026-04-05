1d2eca688065ee82716e84312c6eb8d16a9f18bf
Churchillk <churchilkodhiambo@gmail.com>
2026-04-05 20:33:33 +0300

    htmls

--- Home/views.py
+++ Home/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView
 
-# Create your views here.
+
+class HomeView(TemplateView):
+    template_name = 'Home/home.html'
+    
+class AboutView(TemplateView):
+    template_name = 'Home/about.html'
+    
+class ContactView(TemplateView):
+    template_name = 'Home/contact.html'
+    
+class ServicesView(TemplateView):
+    template_name = 'Home/services.html'
+    
+class TourView(TemplateView):
+    template_name = 'Home/tours.html'
+    
+class BookingView(TemplateView):
+    template_name = 'Home/booking.html'
