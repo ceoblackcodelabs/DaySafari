e185c6a311d920c93ef9f08720bf3cb7eb46b30b
Churchillk <churchilkodhiambo@gmail.com>
2026-04-16 23:10:03 +0300

    other pages

--- Home/urls.py
+++ Home/urls.py
@@ -1,11 +1,34 @@
 from django.urls import path
-from .views import HomeView, AboutView, ContactView, ServicesView, TourView, BookingView
+from .views import (HomeView, AboutView, ContactView, 
+                    ServicesView, TourView, BookingView,
+                    AfricaTourView, EastAfricaTourView, InternationalAfricaTourView,
+                    CruisesView, AirLineView, BlogsView, GalleryView, AirBNBView,
+                    AfricanWildLifeToursView, TravelPartnershipsView, HolidayTailorMadeToursView, AirportTransfersView
+                    )
 
 urlpatterns = [
     path('', HomeView.as_view(), name='home'),
     path('about/', AboutView.as_view(), name='about'),
     path('contact/', ContactView.as_view(), name='contact'),
+    
+    # services
     path('services/', ServicesView.as_view(), name='services'),
+    path('african-wildlife-tours/', AfricanWildLifeToursView.as_view(), name='african_wildlife_tours'),
+    path('travel-partnerships/', TravelPartnershipsView.as_view(), name='travel_partnerships'),
+    path('holiday-tailor-made-tours/', HolidayTailorMadeToursView.as_view(), name='holiday_tailor_made_tours'),
+    path('airport-transfers/', AirportTransfersView.as_view(), name='airport_transfers'),
+    
+    path('cruises/', CruisesView.as_view(), name='cruises'),
+    path('airline/', AirLineView.as_view(), name='airline'),
+    path('blog/', BlogsView.as_view(), name='blog'),
+    path('packages/', GalleryView.as_view(), name='packages'),
+    path('airbnb/', AirBNBView.as_view(), name='airbnb'),
+
+    # tours
     path('tours/', TourView.as_view(), name='tours'),
+    path('africa-tours/', AfricaTourView.as_view(), name='africa_tours'),
+    path('east-africa-tours/', EastAfricaTourView.as_view(), name='east_africa_tours'),
+    path('international-africa-tours/', InternationalAfricaTourView.as_view(), name='international_africa_tours'),
+    # bookings
     path('booking/', BookingView.as_view(), name='booking'),
 ]
--- Home/views.py
+++ Home/views.py
@@ -11,11 +11,49 @@ class AboutView(TemplateView):
 class ContactView(TemplateView):
     template_name = 'Home/contact.html'
     
+# services
 class ServicesView(TemplateView):
     template_name = 'Home/services.html'
     
+class AfricanWildLifeToursView(TemplateView):
+    template_name = 'Services/african_wildlife_tours.html'
+    
+class TravelPartnershipsView(TemplateView):
+    template_name = 'Services/travel_partnerships.html'
+    
+class HolidayTailorMadeToursView(TemplateView):
+    template_name = 'Services/holiday_tailor_made_tours.html'
+    
+class AirportTransfersView(TemplateView):
+    template_name = 'Services/airport_transfers.html'
+
+# tours
 class TourView(TemplateView):
     template_name = 'Home/tours.html'
     
+class AfricaTourView(TemplateView):
+    template_name = 'Tours/africa_tours.html'
+    
+class EastAfricaTourView(TemplateView):
+    template_name = 'Tours/east_africa_tours.html'
+    
+class InternationalAfricaTourView(TemplateView):
+    template_name = 'Tours/international_africa_tours.html'
+    
 class BookingView(TemplateView):
     template_name = 'Home/booking.html'
+    
+class CruisesView(TemplateView):
+    template_name = 'Home/cruises.html'
+    
+class AirLineView(TemplateView):
+    template_name = 'Home/airline.html'
+    
+class BlogsView(TemplateView):
+    template_name = 'Blogs/blogs.html'
+    
+class GalleryView(TemplateView):
+    template_name = 'Home/gallery.html'
+    
+class AirBNBView(TemplateView):
+    template_name = 'Home/airbnb.html'
